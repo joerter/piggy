1f3a68d9e55b5fffc971b8278a6355386a036948
John Oerter <john.oerter@fcsamerica.com>
2017-12-08 10:20:55 -0600

    Add tests for create piggy bank

--- src/app/piggy-bank/create-piggy-bank/create-piggy-bank.component.spec.ts
+++ src/app/piggy-bank/create-piggy-bank/create-piggy-bank.component.spec.ts
@@ -1,25 +1,63 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { CREATE_PIGGY_BANK } from "../piggy-bank.reducer";
+import { CreatePiggyBankComponent } from "./create-piggy-bank.component";
+import { PiggyBank } from "../piggy-bank.model";
+import { Validators } from "@angular/forms";
 
-import { CreatePiggyBankComponent } from './create-piggy-bank.component';
+describe('Create Piggy Bank Component', () => {
+    let component: CreatePiggyBankComponent;
 
-describe('CreatePiggyBankComponent', () => {
-  let component: CreatePiggyBankComponent;
-  let fixture: ComponentFixture<CreatePiggyBankComponent>;
+    const formBuilderSpy = jasmine.createSpyObj('formBuilder', ['group']);
+    const storeSpy = jasmine.createSpyObj('store', ['dispatch']);
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ CreatePiggyBankComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(() => {
+        component = new CreatePiggyBankComponent(formBuilderSpy, storeSpy);
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CreatePiggyBankComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    describe('ngOnInit', () => {
+        it('should create the form', () => {
+            component.ngOnInit();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+            expect(formBuilderSpy.group).toHaveBeenCalledWith({
+                name: ['', Validators.required],
+                amount: ['', Validators.required],
+                goal: ''
+            })
+        });
+    });
+
+    describe('onSubmit', () => {
+        const formValue = {
+            name: 'New Car',
+            amount: 1234.56,
+            goal: 123456.78
+        }
+        beforeEach(() => {
+            component.createPiggyBankForm = <any>{
+                value: formValue,
+                reset: jasmine.createSpy('reset')
+            }
+        });
+
+        it('should reset the form', () => {
+            component.onSubmit();
+
+            expect(component.createPiggyBankForm.reset).toHaveBeenCalledTimes(1);
+        });
+
+        it('should dispatch a CREATE_PIGGY_BANK action with a piggybank payload', () => {
+            component.onSubmit();
+
+            const payload: PiggyBank = {
+                name: formValue.name,
+                amount: formValue.amount,
+                goal: formValue.goal
+            };
+            const expectedAction = {
+                type: CREATE_PIGGY_BANK,
+                payload
+            };
+
+            expect(storeSpy.dispatch).toHaveBeenCalledWith(expectedAction);
+        });
+    });
 });
